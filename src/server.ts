797d37d77f8af0f161db0c81ab9ac27a07f35a96
Giovanni R Santos <giovannirodrigues.2012@gmail.com>
2025-02-18 16:41:04 -0300

    Configurando swagger para documentacao

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,35 @@
 import { fastify } from 'fastify';
 import { fastifyCors } from '@fastify/cors';
-import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod';
+import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod';
+import { fastifySwagger } from '@fastify/swagger';
+import { fastifySwaggerUi } from '@fastify/swagger-ui';
+import { incritosEventoRoute } from './routes/incritos-evento-route';
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(fastifyCors, {
     origin: 'http://localhost:3333'
-})
+});
 
-app.get('/hello', () => {
-    return 'hello word'
-})
+app.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: 'Sistema de indicacao',
+            version: '0.0.1'
+        }
+    },
+    transform: jsonSchemaTransform
+});
+
+app.register(fastifySwaggerUi, {
+    routePrefix: '/docs'
+});
+
+app.register(incritosEventoRoute);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
-})
+});
