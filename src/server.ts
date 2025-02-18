21184c3916a9cf67a7427246302e7c9032e27db7
Giovanni R Santos <giovannirodrigues.2012@gmail.com>
2025-02-18 16:16:52 -0300

    Adicionando validacao e serializacao

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import { fastify } from 'fastify';
 import { fastifyCors } from '@fastify/cors';
+import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod';
 
-const app = fastify()
+const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 app.register(fastifyCors, {
     origin: 'http://localhost:3333'
